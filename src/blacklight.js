e94307158e21874a7d7c94b10e35a439ea9e2473
Justin Coyne <jcoyne@justincoyne.com>
2018-12-30 16:30:28 -0600

    Install progress bar

--- src/blacklight.js
+++ src/blacklight.js
@@ -1,11 +1,28 @@
 import Search from './components/Search';
 import ShowPage from './components/ShowPage';
+import VueProgressBar from 'vue-progressbar'
+
+const progressOptions = {
+  color: '#e62117',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  inverse: false
+}
 
 // This is your plugin object. It can be exported to be used anywhere.
 const Blacklight = {
+
+
   // The install method is all that needs to exist on the plugin object.
   // It takes the global Vue object as well as user-defined options.
   install(Vue, options) {
+    Vue.use(VueProgressBar, progressOptions)
 
     // We call Vue.mixin() here to inject functionality into all components.
   	Vue.mixin({
