ed71f9faf0a4b634824380325c8889c37ec2a4fa
Justin Coyne <jcoyne@justincoyne.com>
2018-12-30 17:00:09 -0600

    Export Routes correctly

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Search from './components/Search';
 import ShowPage from './components/ShowPage';
 
-export const Routes = [
+const Routes = [
   { path: '/item/:id', name: 'show', component: ShowPage },
   { path: '/catalog/:filter?', name: 'search', component: Search }
 ]
+
+export default Routes
